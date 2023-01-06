c74fdfcc5597df19ddde56ca48d133453ae12916
ca-alarcon <121892606+ca-alarcon@users.noreply.github.com>
2023-01-06 18:53:07 -0300

    Create CodeJupyterNotebook.py
    This is how it has worked.

---
+++ CodeJupyterNotebook.py
@@ -0,0 +1,34 @@
+# My Jupyter Notebook on IBM Watson Studio
+**Carlos Antonio Alarcón Villegas**
+<br>
+Desired occupation: Industrial Engineer.
+***I am interested in data science because it allows me to solve complex problems using a combination of statistical analysis and computer programming.***
+### Code in Cell 5
+The code in Cell 5 will compute the sum of two numbers:
+def sum (a,b):
+    return a+b
+#For example:
+x = 1
+y = 2
+z = sum (x,y)
+print(z)
+
+Bulleted list:
+
+* Item A 
+* Item B 
+* Item C
+<br>
+
+Numbered list:
+1. Item A 
+2. Item B 
+3. Item C
+<br>
+
+Table:
+
+| Col A | Col B | 
+|--------------|--------------| 
+| A1 | B1 | 
+| A2 | B2 |
