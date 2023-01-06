abe18ed62bdd1caf6f2aee4f26bb671343b6374b
ca-alarcon <121892606+ca-alarcon@users.noreply.github.com>
2023-01-06 15:57:46 -0300

    Create project.py

---
+++ project.py
@@ -0,0 +1,42 @@
+# My Jupyter Notebook on IBM Watson Studio
+<br>
+**Carlos Antonio Alarcón Villegas** 
+<br>
+<br>
+Desired occupation: Industrial Engineer.
+***I am interested in data science because it allows me to solve complex problems using a combination of statistical analysis and computer programming.***
+<br>
+### Code in Cell 5
+<br>
+The code in Cell 5 will compute the sum of two numbers:
+<br>
+def sum (a,b):
+    return a+b
+#For example:
+x = 1
+y = 2
+z = sum (x,y)
+print(z)
+<br>
+Horizontal rule: 
+<br>
+---
+<br>
+Bulleted list: 
+<br>
+* Item A
+* Item B
+* Item C
+<br>
+Numbered list:
+<br>
+1. Item A
+2. Item B
+3. Item C
+<br>
+Table:
+<br>
+| Encabezado 1 | Encabezado 2 |
+|--------------|--------------|
+| Celda 1      | Celda 2      |
+| Celda 3      | Celda 4      |
